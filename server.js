1138028bae0574db3aad0aa05cd66f7c39332938
Vincent Hong <vinlegend1@gmail.com>
2020-08-24 10:44:12 +0800

    Fixing some stuff

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get('/', (req, res) => {
 app.use('/memes', require('./routes/memes'));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/client/build')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
